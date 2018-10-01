ab87dfb5ed921ae7ad3023006bf7f2bf570c4a0f
kmlee95 <kmlee95@naver.com>
2018-10-02 01:12:52 +0900

    텍스트입력

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -4,10 +4,21 @@ import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 class App extends Component{
+    state = {
+        input: ''
+    }
+    handleChange = (e) =>{
+        const {value} = e.target;
+        this.setState({
+            input: value
+        });
+    }
     render(){
+        const { input } = this.state;
+        const { handleChange } = this; // 아래에 this.handleChange처럼 계속 반복해서 안쓰기 위해
         return(
            <PageTemplate>
-               <TodoInput/>
+               <TodoInput onChange={handleChange}  value={input}/> 
                <TodoList/>
            </PageTemplate>
         );
