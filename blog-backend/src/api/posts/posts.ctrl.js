e2eea95dd236d92c93187c828954711246cb63d6
LeeKyungmin <kmlee95@naver.com>
2019-02-21 12:18:27 +0900

    컨트롤러

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -7,11 +7,87 @@ const posts = [
     }
 ];
 
+//포스트 작성 부분
 exports.write = (ctx) =>{
-    const{
-        title,
-        body
-    } = ctx.request.body;
-}
+    const { title,body } = ctx.request.body;
 
-peostID += 1;
+    postId += 1;
+
+    const post = { id: postId, title, body };
+    posts.push(post);
+    ctx.body = post;
+};
+
+//포스트 목록 조회 GET /api/posts
+exports.list = (ctx) =>{
+    ctx.body = posts;
+};
+
+//특정 포스트 목록 조회 GET /api/posts/:id
+exports.read = (ctx) =>{
+    const {id} = ctx.params;
+    const post = posts.find(p=>p.id.toString() === id);
+
+    if(!post){
+        ctx.status = 404;
+        ctx.body = {
+            message: '포스트가 존재하지 않습니다'
+        };
+        return ;
+    }
+    ctx.body = post;
+};
+
+//특정 포스트 제거 DELETE /api/posts/:id
+exports.remove = (ctx) =>{
+    const {id} = ctx.params;
+    const index = posts.findIndex(p=>p.id.toString() === id);
+
+    if(index === -1){
+        ctx.status = 404;
+        ctx.body ={
+            message: '포스트가 존재하지 않습니다'
+        };
+        return;
+    }
+    posts.splice(index, 1);
+    ctx.status = 204;
+};
+
+//포스트 수정 교체 PUT /api/posts/:id
+
+exports.replace = (ctx)=>{
+    const {id} = ctx.params;
+    const index = posts.findIndex(p=>p.id.toString() === id);
+    if(index === -1){
+        ctx.status = 404;
+        ctx.body ={
+            message: '포스트가 존재하지 않습니다'
+        };
+        return;
+    }
+
+    posts[index] = {
+        id,
+        ...ctx.request.body
+    };
+    ctx.body = posts[index];
+};
+
+//포스트 수정(특정 필드 변경) PATH /api/posts/:id
+exports.update = (ctx) =>{
+    const{id} = ctx.params;
+    const index = posts.findIndex(p=>p.id.toString() === id);
+    if(index === -1){
+        ctx.status = 404;
+        ctx.body ={
+            message: '포스트가 존재하지 않습니다'
+        };
+        return;
+    }
+    posts[index] ={
+        ...posts[index],
+        ...ctx.request.body
+    };
+    ctx.body = posts[index];
+};
