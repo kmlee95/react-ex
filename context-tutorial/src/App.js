850942d89cd670c24fd89ead8e19eff2c2dfaf6f
kmlee95 <kmlee95@naver.com>
2019-02-24 15:32:52 +0900

    commit

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import LeftPane from './components/LeftPane';
 import RightPane from './components/RightPane';
 import { SampleProvider } from './contexts/sample';
+import { AnotherProvider } from './contexts/another';
 
 const App = () =>{
   return(
     <SampleProvider>
-      <div className="panes">
-        <LeftPane />
-        <RightPane />
-      </div>
+      <AnotherProvider>
+        <div className="panes">
+          <LeftPane />
+          <RightPane />
+        </div>
+      </AnotherProvider>
     </SampleProvider>
   );
 }
